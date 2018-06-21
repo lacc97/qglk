b02c3dddfc0fbec6a7d65da0cceff5cbd66db67d
Luis Cáceres <lacaceres97@gmail.com>
2018-06-21 17:28:00 -0500

    Fix gidispatch object registry

--- src/qglk_dispatch.cpp
+++ src/qglk_dispatch.cpp
@@ -1,6 +1,12 @@
 #include "glk.hpp"
 
+#include <QSet>
 #include <QHash>
+
+#include "file/fileref.hpp"
+#include "sound/schannel.hpp"
+#include "stream/stream.hpp"
+#include "window/window.hpp"
 
 gidispatch_rock_t (*s_ObjectRegisterFunction)(void* obj, glui32 objclass) = NULL;
 void (*s_ObjectUnregisterFunction)(void* obj, glui32 objclass, gidispatch_rock_t objrock) = NULL;
@@ -8,7 +14,6 @@ void (*s_ObjectUnregisterFunction)(void* obj, glui32 objclass, gidispatch_rock_t
 QHash<void*, gidispatch_rock_t> s_ArrayHash;
 gidispatch_rock_t (*s_ArrayRegisterFunction)(void* array, glui32 len, char* typecode) = NULL;
 void (*s_ArrayUnregisterFunction)(void* array, glui32 len, char* typecode, gidispatch_rock_t objrock) = NULL;
-
 
 void Glk::Dispatch::registerObject(Glk::Object* ptr) {
     if(s_ObjectRegisterFunction)
@@ -45,6 +50,20 @@ void Glk::Dispatch::unregisterArray(void* ptr, glui32 len, bool unicode) {
 void gidispatch_set_object_registry(gidispatch_rock_t (*reg)(void* obj, glui32 objclass), void (*unreg)(void* obj, glui32 objclass, gidispatch_rock_t objrock)) {
     s_ObjectRegisterFunction = reg;
     s_ObjectUnregisterFunction = unreg;
+
+    if(s_ObjectRegisterFunction) {
+        for(frefid_t fref = glk_fileref_iterate(NULL, NULL); fref; fref = glk_fileref_iterate(fref, NULL))
+            Glk::Dispatch::registerObject(FROM_STRID(fref));
+
+        for(schanid_t schan = glk_schannel_iterate(NULL, NULL); schan; schan = glk_schannel_iterate(schan, NULL))
+            Glk::Dispatch::registerObject(FROM_SCHANID(schan));
+
+        for(strid_t str = glk_stream_iterate(NULL, NULL); str; str = glk_stream_iterate(str, NULL))
+            Glk::Dispatch::registerObject(FROM_STRID(str));
+
+        for(winid_t win = glk_window_iterate(NULL, NULL); win; win = glk_window_iterate(win, NULL))
+            Glk::Dispatch::registerObject(FROM_WINID(win));
+    }
 }
 
 gidispatch_rock_t gidispatch_get_objrock(void* obj, glui32 objclass) {
