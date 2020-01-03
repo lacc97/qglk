4c04cdcac66947bd69decb3dea3cc9808f2b2897
Luis Cáceres <lacaceres97@gmail.com>
2020-01-03 16:18:10 -0500

    Small improvement to TextGridWidget drawing

--- src/window/textgridwidget.cpp
+++ src/window/textgridwidget.cpp
@@ -20,15 +20,19 @@ void Glk::TextGridWidget::paintEvent(QPaintEvent* event) {
     QWidget::paintEvent(event);
 
     QRect unitBox(0, 0, contentsRect().width() / m_Grid.size(), contentsRect().height() / m_Grid[0].size());
-    int glyphHMargin = (unitBox.width() - fontMetrics().horizontalAdvance('m')) / 2;
     int glyphVMargin = (unitBox.height() - fontMetrics().height()) / 2;
-    QRect glyphBox(glyphHMargin, glyphVMargin, fontMetrics().horizontalAdvance('m'), fontMetrics().height());
 
     std::unique_ptr<QPainter> p = std::make_unique<QPainter>(this);
 
     for(size_t xx = 0; xx < m_Grid.size(); ++xx) {
         for(size_t yy = 0; yy < m_Grid[xx].size(); ++yy) {
-            QRect thisGlyphBox = glyphBox.translated(xx * unitBox.width(), yy * unitBox.height());
+            QString ch = QString::fromUcs4(&m_Grid[xx][yy], 1);
+            int hAdvance = fontMetrics().horizontalAdvance(ch);
+
+            QRect thisUnitBox = unitBox.translated(xx * unitBox.width(), yy * unitBox.height());
+            int glyphHMargin = (unitBox.width() - hAdvance) / 2;
+            QRect thisGlyphBox(thisUnitBox.x() + glyphHMargin, thisUnitBox.y() + glyphVMargin,
+                               hAdvance, fontMetrics().height());
 
             if(event->rect().intersects(thisGlyphBox))
                 p->drawText(QPoint(thisGlyphBox.x(), thisGlyphBox.y() + fontMetrics().ascent()),
