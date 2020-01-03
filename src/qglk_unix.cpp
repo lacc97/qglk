ba9b866dd657ff072cc7ab8d6acb869e62edf7c0
Luis Cáceres <lacaceres97@gmail.com>
2020-01-03 18:31:50 -0500

    Use spdlog in glk_unix.cpp

--- src/qglk_unix.cpp
+++ src/qglk_unix.cpp
@@ -4,43 +4,38 @@ extern "C" {
 #include "glkstart.h"
 }
 
-#include <QDebug>
 #include <QDir>
 #include <QFileInfo>
+
+#include "log/log.hpp"
 
 #include "stream/latin1stream.hpp"
 
 void glkunix_set_base_file(char* filename) {
+    SPDLOG_TRACE("glkunix_set_base_file({0})", filename);
+
     QFileInfo fi(filename);
 
     QDir::setCurrent(fi.absoluteDir().absolutePath());
 }
 
 strid_t glkunix_stream_open_pathname(char* pathname, glui32 textmode, glui32 rock) {
-#ifndef NDEBUG
-    QDebug deb = qDebug();
-    deb << "glkunix_stream_open_pathname(" << QString(pathname) << "," << textmode << "," << rock << ") =>";
-#endif
+    SPDLOG_TRACE("glkunix_stream_open_pathname({0}, {1}, {2})", pathname, (bool)textmode, rock);
 
     QFileInfo path(pathname);
 
     if(!path.exists() || path.isDir())
         return NULL;
 
-    Glk::Stream* str = new Glk::Latin1Stream(NULL, new QFile(path.absoluteFilePath()), Glk::Stream::Type::File, rock);
     QIODevice::OpenMode om = QIODevice::ReadOnly;
+    if(textmode)
+        om |= QIODevice::Text;
 
+    Glk::Stream* str = new Glk::Latin1Stream(NULL, new QFile(path.absoluteFilePath()), Glk::Stream::Type::File, rock);
     if(!str->open(om)) {
         delete str;
-#ifndef NDEBUG
-        deb << ((void*)NULL);
-#endif
         return NULL;
     }
-
-#ifndef NDEBUG
-    deb << TO_STRID(str);
-#endif
 
     return TO_STRID(str);
 }
