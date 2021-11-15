ae2df47c3d7d71c5dfbf460fbd7a6a253da3d0f1
Luis Cáceres <lacc97@protonmail.ch>
2021-11-15 21:16:15 +0000

    Write formatter for std::filesystem::path

--- src/log/log.hpp
+++ src/log/log.hpp
@@ -2,6 +2,7 @@
 #define LOG_HPP
 
 #include <chrono>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -26,6 +27,19 @@ namespace fmt {
         template <typename FormatContext>
         auto format(const QString& str, FormatContext &ctx) {
             return format_to(ctx.out(), "\"{0}\"", str.toStdString());
+        }
+    };
+
+    template<>
+    struct formatter<std::filesystem::path> {
+        template <typename ParseContext>
+        constexpr auto parse(ParseContext &ctx) {
+            return ctx.begin();
+        }
+
+        template <typename FormatContext>
+        auto format(const std::filesystem::path& p, FormatContext &ctx) {
+            return format_to(ctx.out(), "\"{0}\"", p.generic_string());
         }
     };
 }
